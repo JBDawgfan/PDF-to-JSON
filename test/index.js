d0e216724de23bbd925c1ae22fe101dc9a369a27
JBDawgfan <johnm.bradley@comcast.net>
2017-06-18 12:49:14 -0700

    Changes

--- test/index.js
+++ test/index.js
@@ -1,22 +1,42 @@
-
 /**
- * Test Descriptions
- */
+	 * Requirements
+	 */
+	
 
-describe('resumePdfToJson', function() {
+	var chai = require('chai'),
+	    assert = require('chai').assert,
+	    chaiAsPromised = require('chai-as-promised');
+	var resumePdfToJson = require('../index');
+	
+
+	
+
+	chai.use(chaiAsPromised);
+	
+
+	
+
+	/**
+	 * Test Descriptions
+	 */
+	
+
+		
+
+	    var path = 'JohnBradley.pdf';
+	    var output = 'JohnBradley.json';
+	
+
+	    var p = resumePdfToJson(path, {'output': output})
+	        .then(function(data) {
+	            return data;
+	        });
+	
+	   /*var p = resumePdfToJson(path)
+	        .then(function(data) {
+	            return data;
+	        });*/
 
 
-    var path = 'test/JohnBradleyProfile.pdf';
-    var output = 'test/JohnBradley.json';
 
-    var p = resumePdfToJson(path, {'output': output})
-        .then(function(data) {
-            return data;
-        });
-
-   
-    });
-
-
-});
 
